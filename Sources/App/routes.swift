fbab7fb7cf096905012c2bc9d5e8e3920566e8c3
Douglas Hewitt <doughewitt4@gmail.com>
2021-04-25 02:49:50 -0700

    Update routes.swift
    basic sensor demo for app or arduino to post sensor data

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -9,6 +9,21 @@ func routes(_ app: Application) throws {
     app.get("hello") { req -> String in
         return "Hello, world!"
     }
+    
+    app.get("gardener", ":name") { req -> String in
+        let name = req.parameters.get("name")!
+        return "Hello, \(name)!"
+    }
+    
+    app.post("gardener", ":name", ":plant-name", "light-sensor-data") { req -> LightSensorData in
+        let sensorData = try req.content.decode(LightSensorData.self)
+        return sensorData
+    }
 
     try app.register(collection: TodoController())
+}
+
+struct LightSensorData: Content {
+    let value: Int
+    let timestamp: Date
 }
